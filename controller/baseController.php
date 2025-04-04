40ae749b333fac4f522a169c65ea13016944637f
giusmili <giusmili67@gmail.com>
2025-04-04 15:21:02 +0200

    php

--- controller/baseController.php
+++ controller/baseController.php
@@ -1,13 +1,13 @@
 <?php
-    // Requête pour récupérer les données de la table
+    # Requête pour récupérer les données de la table
    
     
     $sql = "SELECT nom, type, version, url FROM donaldtablemusksaloperie";
     $stmt = $pdo->query($sql);
     
-    // Vérifier si des données sont retournées
+    # Vérifier si des données sont retournées
     if ($stmt->rowCount() > 0) {
-        // Récupérer les données sous forme de tableau associatif
+        # Récupérer les données sous forme de tableau associatif
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     } else {
         $rows = [];
