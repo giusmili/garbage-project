4edddd93fde1601da99af69f24944b548c8c293d
giusmili <giusmili67@gmail.com>
2025-04-04 14:44:48 +0200

    php

---
+++ partial/header.inc.php
@@ -0,0 +1,5 @@
+<header>
+        <h1>
+            <span aria-hidden="true">💪</span>Vibe coding
+        </h1>
+    </header>
---
+++ partial/section.inc.php
@@ -0,0 +1,14 @@
+    <section>
+        <h2>
+            <span aria-hidden="true">🚀</span>Description
+        </h2>
+            <p>
+                Before lunch several of the passengers organized a pool, which could not fail to please those fond of
+                betting and gambling. The result of this pool was not to be known for four days; it was what is called
+                the 'pilot's pool.' When a ship arrives at the land-falls every one knows that a pilot comes on board;
+                so they divide the twenty-four hours
+                of the day and night into forty-eight half-hours
+            </p>
+            <?php  include_once 'figure.inc.php'?>
+      
+    </section>
