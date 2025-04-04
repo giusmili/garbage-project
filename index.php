55d0e188c36b59a6209391d300cd287e345ac090
giusmili <giusmili67@gmail.com>
2025-04-04 16:14:01 +0200

    php

--- index.php
+++ index.php
@@ -5,19 +5,9 @@ include_once __DIR__ .'/partial/head.inc.php'
 ?>
 
 <body>
-   <!--  header.php -->
-   <?php include_once __DIR__ .'/partial/header.inc.php' ?>
-    <!-- main.php -->
-    
-    <main>
-    
-        <!-- partials/description-section.php -->
-         <?php include_once __DIR__ .'/partial/section.inc.php' ?>
-       
-    </main>
-  
-    <!--  footer -->
-    <?php include_once __DIR__ .'/partial/footer.inc.php' ?>
+<?php
+include_once __DIR__ .'/view/presentation.php';
+?>
 </body>
 
-</html>
+</html> 
---
+++ view/presentation.php
@@ -0,0 +1,15 @@
+
+
+<!--  header.php -->
+<?php include_once 'partial/header.inc.php' ?>
+ <!-- main.php -->
+ 
+ <main>
+ 
+     <!-- partials/description-section.php -->
+      <?php include_once 'partial/section.inc.php' ?>
+    
+ </main>
+
+ <!--  footer -->
+ <?php include_once 'partial/footer.inc.php' ?>
